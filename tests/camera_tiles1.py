777d7f124549488bcdfaaf8eee161a06bd189be9
Kurtis Fields <kurtisfields@gmail.com>
2022-12-03 03:24:47 -0700

    feat: camera_tiles.py

---
+++ tests/camera_tiles1.py
@@ -0,0 +1,146 @@
+import math
+import arcade
+import glm
+
+from deeper import Space, Cuboid, Ray
+
+degRads = (math.pi * 2) / 360
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+CELL_WIDTH = 216
+CELL_HEIGHT = 216
+CELL_DEPTH = 252
+"""
+Let's try creating sprites and batching them according to bounding box z-order
+Thinking about doing away with the grid ...
+"""
+
+"""
+proj = glm.ortho(-1, 1, -1, 1, -1.0, 1.0)
+inv_proj = glm.inverse(proj)
+
+view = glm.mat4(1)
+view = glm.rotate(view, -57 * degRads, glm.vec3(1, 0, 0))
+view = glm.rotate(view, -30 * degRads, glm.vec3(0, 1, 0))
+inv_view = glm.inverse(view)
+"""
+
+class WorldCamera:
+    def __init__(self, target, distance):
+        self.target = target
+        #self.direction = glm.normalize(self.position - target)
+        #self.view = glm.lookAt(position, target, glm.vec3(0.0, 1.0, 0.0))
+        self.view = glm.rotate(glm.mat4(1), -57 * degRads, glm.vec3(1, 0, 0))
+        self.view = glm.rotate(self.view, -30 * degRads, glm.vec3(0, 1, 0))
+        self.inv_view = glm.inverse(self.view)
+        self.orientation = glm.quat(self.inv_view)
+        self.proj = glm.ortho(-1, 1, -1, 1, -1.0, 1.0)
+        self.inv_proj = glm.inverse(self.proj)
+        #self.direction = glm.normalize(self.inv_proj * self.inv_view * glm.vec3(0.0, 0.0, 1.0))
+        #self.direction = self.orientation * glm.vec3(0.0, 0.0, distance)
+        #self.position = glm.vec3(95, 295, 165)
+        self.position = (self.orientation * glm.vec3(0.0, 0.0, -distance)) + target
+        #self.position = glm.inverse(self.orientation) * glm.vec3(0.0, 0.0, distance)
+        zoom=1.5
+        #zoom=1
+        self.camera = arcade.Camera(zoom=zoom)
+        self.camera.move(self.project(glm.mat4(1), target).xy)
+
+    def use(self):
+        self.camera.use()
+
+    def project(self, model, point):
+        return glm.vec3(self.proj * self.view * model * point)
+
+    def mouse_to_ray(self, ray_nds):
+        ray_clip = glm.vec4(ray_nds.xy, -1.0, 1.0)
+        ray_eye = self.inv_proj * ray_clip
+        print("camera position: ", self.position)
+        print("ray_eye: ", ray_eye)
+        ray_world = (self.inv_view * ray_eye).xyz
+        #ray_world = glm.normalize(ray_world)
+        ray_world = glm.normalize((self.orientation * ray_eye).xyz)
+        print("ray_world: ", ray_world)
+        ray = Ray(*self.position, *ray_world)
+        return ray
+
+class Selection:
+    def __init__(self, position):
+        self.position = position
+
+
+class Deeper(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Deeper", resizable=True)
+        #self.position = glm.vec3(95, 295, 165)
+        #self.camera = WorldCamera(glm.vec3(0, 0, 0), 200)
+        self.camera = WorldCamera(glm.vec3(CELL_WIDTH*4, 0, CELL_DEPTH*-4), 500)
+        self.tiles = arcade.SpriteList()
+
+        self.space = Space()
+        self.selection = None
+
+        self.create_boxes()
+        self.create_sprites()
+
+    def create_boxes(self):
+        rotation = glm.vec3()
+        shape = Cuboid(CELL_WIDTH, 16, CELL_DEPTH)
+        for ty in range(0, 8):
+            y_distance = CELL_DEPTH * ty
+            for tx in range(0, 8):
+                x_distance = CELL_WIDTH * tx
+                self.space.add_child(
+                    Space(glm.vec3(x_distance, 16, -y_distance), rotation, shape)
+                )
+
+    def create_sprites(self):
+        sorted_spaces = sorted(self.space.children, key=lambda space: space.position[2])
+        for space in sorted_spaces:
+            sprite = arcade.Sprite("../resources/tiles/Floor8a.png")
+            model = glm.mat4(1)
+            pos = self.camera.project(model, space.position)
+            sprite.set_position(pos[0], pos[2])
+            self.tiles.append(sprite)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.camera.use()
+        self.tiles.draw()
+
+        model = glm.mat4(1)
+        pos = self.camera.project(model, self.camera.position)
+        arcade.draw_circle_outline(pos[0], pos[1], 18, arcade.color.WISTERIA, 3)
+
+        if self.selection:
+            model = glm.mat4(1)
+            pos = self.camera.project(model, self.selection.position)
+            arcade.draw_line(0, 0, pos[0], pos[1], arcade.color.YELLOW, 2)
+        arcade.finish_render()
+
+    def on_mouse_motion(self, mouse_x, mouse_y, mouse_dx, mouse_dy):
+        width, height = self.get_size()
+        #normalize device coordinates
+        x = (2.0 * mouse_x) / width - 1.0
+        #y = 1.0 - (2.0 * mouse_y) / height
+        y = (2.0 * mouse_y) / height - 1.0
+        z = 1.0
+        print("nds: ", x, y, z)
+        ray_nds = glm.vec3(x, y, z)
+        ray = self.camera.mouse_to_ray(ray_nds)
+        contact = self.space.cast_ray(ray)
+        print(contact)
+        if contact:
+            self.selection = Selection(glm.vec3(contact))
+        else:
+            self.selection = None
+
+def main():
+    window = Deeper()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
