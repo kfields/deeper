e47a9b60889f781241515e488e3784b51e7ed573
Kurtis Fields <kurtisfields@gmail.com>
2023-01-27 15:07:13 -0700

    fix: widget sort

--- deeper/dimgui/widget_group.py
+++ deeper/dimgui/widget_group.py
@@ -1,3 +1,5 @@
+from loguru import logger
+
 import imgui
 
 from . import Widget
@@ -15,17 +17,28 @@ class WidgetGroup(Widget):
             self.draw_child(child)
             if imgui.is_item_hovered(imgui.HOVERED_RECT_ONLY):
                 hovered_index = i
-
+        """
         if hovered_index > -1 and self.dragging:
             if imgui.is_mouse_dragging():
                 if hovered_index != self.drag_index:
-                    self.swap(hovered_index, self.drag_index)
+                    self.swap(self.drag_index, hovered_index)
                     self.drag_index = hovered_index
 
-        elif imgui.is_mouse_down(0):
+        elif hovered_index > -1 and imgui.is_mouse_down(0):
+            logger.debug(hovered_index)
             self.drag_index = hovered_index
             #self.dragging = True
-            self.dragging = self.children[i]
+            self.dragging = self.children[hovered_index]
+        """
+        if hovered_index > -1:
+            if self.dragging and imgui.is_mouse_dragging():
+                if hovered_index != self.drag_index:
+                    self.swap(self.drag_index, hovered_index)
+                    self.drag_index = hovered_index
+
+            elif imgui.is_mouse_down(0):
+                self.drag_index = hovered_index
+                self.dragging = self.children[hovered_index]
 
         if not imgui.is_mouse_down(0) and self.dragging:
             self.drag_index = -1
