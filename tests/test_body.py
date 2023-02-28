98299297fe7bb4d2dd67fbf7b4435b0d82b02fab
Kurtis Fields <kurtisfields@gmail.com>
2023-02-28 02:46:13 -0700

    v0.1.7

--- tests/test_body.py
+++
@@ -1,6 +0,0 @@
-from deeper import Body
-
-body = Body((0, 1, 2))
-#center = body.center
-center = body.get_center()
-print(center)
--- tests/test_space.py
+++
@@ -1,6 +0,0 @@
-from deeper import Isometry, Block
-
-child = Block(Isometry(0, 1, 2))
-block = Block(Isometry())
-block.add_child(child)
-print(block.children)
